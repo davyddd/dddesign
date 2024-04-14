ce966df60455cc8aeee35eb53667b724ee41d6c4
davyd_davyd <adavydov@adapty.io>
2024-04-14 19:36:11 +0400

    added tests for BaseError component

---
+++ tests/structure/domains/errors/test_base_error.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from dddesign.structure.domains.errors.base_error import BaseError
+
+
+class TestBaseError(TestCase):
+    def test_correct_state(self):
+        # Act
+        error = BaseError(
+            message='Error message: {error_message}',
+            error_code='some_error',
+            status_code=400,
+            field_name='some_field',
+            error_message='invalid value',
+        )
+
+        # Assert
+        self.assertEqual(error.message, 'Error message: invalid value')
+        self.assertEqual(error.error_code, 'some_error')
+        self.assertEqual(error.status_code, 400)
+        self.assertEqual(error.field_name, 'some_field')
+
+    def test_correct_state_with_default_values(self):
+        # Act
+        error = BaseError(message='Error message: {error_message}', error_message='invalid value')
+
+        # Assert
+        self.assertEqual(error.message, 'Error message: invalid value')
+        self.assertEqual(error.error_code, 'base_error')
+        self.assertEqual(error.status_code, 400)
+        self.assertEqual(error.field_name, None)
+
+    def test_base_error_without_message(self):
+        with self.assertRaises(ValueError):
+            BaseError()
