014270d7921966ee01247f05f3a39def4f334ed0
davyd_davyd <adavydov@adapty.io>
2024-05-17 21:11:45 +0400

    added some comments

--- tests/utils/type_helpers/test_get_type_without_optional.py
+++ tests/utils/type_helpers/test_get_type_without_optional.py
@@ -1,29 +1,34 @@
-import unittest
 from typing import List, Optional, Union
+from unittest import TestCase
 
 from dddesign.utils.type_helpers import get_type_without_optional
 
 
-class TestGetTypeWithoutOptional(unittest.TestCase):
+class TestGetTypeWithoutOptional(TestCase):
     def test_optional_type(self):
+        # Act & Assert
         self.assertEqual(get_type_without_optional(Optional[int]), int)
         self.assertEqual(get_type_without_optional(Optional[str]), str)
         self.assertEqual(get_type_without_optional(Optional[List[int]]), List[int])
 
     def test_non_optional_type(self):
+        # Act & Assert
         self.assertEqual(get_type_without_optional(int), int)
         self.assertEqual(get_type_without_optional(str), str)
         self.assertEqual(get_type_without_optional(List[int]), List[int])
 
     def test_union_with_none(self):
+        # Act & Assert
         self.assertEqual(get_type_without_optional(Union[int, None]), int)
         self.assertEqual(get_type_without_optional(Union[str, None]), str)
         self.assertEqual(get_type_without_optional(Union[List[int], None]), List[int])
 
     def test_union_without_none(self):
+        # Act & Assert
         self.assertEqual(get_type_without_optional(Union[int, str]), Union[int, str])
         self.assertEqual(get_type_without_optional(Union[List[int], dict]), Union[List[int], dict])
 
     def test_invalid_type(self):
+        # Act & Assert
         self.assertEqual(get_type_without_optional(None), None)
         self.assertEqual(get_type_without_optional(123), 123)
