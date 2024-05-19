7426fd9ff348346096d9cf664146391ad029f2a5
davyd_davyd <adavydov@adapty.io>
2024-05-19 22:34:50 +0400

    added supporting python versions 3.11 and 3.12

--- dddesign/utils/type_helpers.py
+++ dddesign/utils/type_helpers.py
@@ -1,4 +1,5 @@
 import inspect
+import sys
 from datetime import date, datetime, time, timedelta
 from decimal import Decimal
 from ipaddress import IPv4Address, IPv6Address
@@ -57,25 +58,51 @@ def get_python_type(type_) -> Type[PythonType]:
 def get_new_function(func: Callable, new_name: Optional[str] = None) -> Callable:
     base_code = func.__code__
     func_name = new_name or base_code.co_name
-    new_code = CodeType(
-        base_code.co_argcount,
-        base_code.co_posonlyargcount,
-        base_code.co_kwonlyargcount,
-        base_code.co_nlocals,
-        base_code.co_stacksize,
-        base_code.co_flags,
-        base_code.co_code,
-        base_code.co_consts,
-        base_code.co_names,
-        base_code.co_varnames,
-        base_code.co_filename,
-        func_name,
-        base_code.co_firstlineno,
-        base_code.co_lnotab,
-        base_code.co_freevars,
-        base_code.co_cellvars,
-    )
+
+    if sys.version_info >= (3, 11):
+        # for python 3.11 and newer
+        new_code = CodeType(
+            base_code.co_argcount,
+            base_code.co_posonlyargcount,
+            base_code.co_kwonlyargcount,
+            base_code.co_nlocals,
+            base_code.co_stacksize,
+            base_code.co_flags,
+            base_code.co_code,
+            base_code.co_consts,
+            base_code.co_names,
+            base_code.co_varnames,
+            base_code.co_filename,
+            func_name,
+            base_code.co_firstlineno,
+            base_code.co_linetable,
+            base_code.co_exceptiontable,
+            base_code.co_freevars,
+            base_code.co_cellvars,
+        )
+    else:
+        # for python 3.10 and older
+        new_code = CodeType(
+            base_code.co_argcount,
+            base_code.co_posonlyargcount,
+            base_code.co_kwonlyargcount,
+            base_code.co_nlocals,
+            base_code.co_stacksize,
+            base_code.co_flags,
+            base_code.co_code,
+            base_code.co_consts,
+            base_code.co_names,
+            base_code.co_varnames,
+            base_code.co_filename,
+            func_name,
+            base_code.co_firstlineno,
+            base_code.co_lnotab,
+            base_code.co_freevars,
+            base_code.co_cellvars,
+        )
+
     closure = func.__closure__
     if closure:
         closure = tuple(CellType(cell.cell_contents) for cell in closure)
+
     return FunctionType(new_code, func.__globals__, func_name, func.__defaults__, closure)
