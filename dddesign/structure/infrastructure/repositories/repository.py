c8bdc92bcec001fd5f8c18327d000fede1c175c4
davyd_davyd <adavydov@adapty.io>
2024-07-21 16:39:38 +0400

    added validation method names for Repository component

--- dddesign/structure/infrastructure/repositories/repository.py
+++ dddesign/structure/infrastructure/repositories/repository.py
@@ -1,7 +1,22 @@
+import inspect
+from typing import Generator, Set
+
 from pydantic import BaseModel
 
 
 class Repository(BaseModel):
+    ALLOWED_METHODS: Set[str] = {'get', 'get_list', 'create', 'update', 'delete'}
+
     class Config:
         allow_mutation = False
         arbitrary_types_allowed = True
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
+        allowed_methods = cls.__fields__['ALLOWED_METHODS'].get_default()
+        methods: Generator[str, ..., ...] = (name for name, member in cls.__dict__.items() if inspect.isfunction(member))
+
+        for method in methods:
+            if method not in allowed_methods:
+                raise TypeError(f'Method name `{method}` does not allowed')
