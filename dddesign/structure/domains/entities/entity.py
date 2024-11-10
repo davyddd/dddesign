d0ac2eebb4d79e918eff7c1fb42ffbc387dd188f
davyd_davyd <adavydov@adapty.io>
2024-11-10 17:11:54 +0400

    added exclude_fields argument to the update method of Entity

--- dddesign/structure/domains/entities/entity.py
+++ dddesign/structure/domains/entities/entity.py
@@ -1,3 +1,5 @@
+from typing import Optional, Set
+
 from pydantic import BaseModel, ConfigDict
 
 from dddesign.structure.domains.dto.dto import DataTransferObject
@@ -6,7 +8,7 @@ from dddesign.structure.domains.dto.dto import DataTransferObject
 class Entity(BaseModel):
     model_config = ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
 
-    def update(self, data: DataTransferObject):
-        for field_name, value in data.model_dump(exclude_unset=True).items():
+    def update(self, data: DataTransferObject, exclude_fields: Optional[Set[str]] = None):
+        for field_name, value in data.model_dump(exclude_unset=True, exclude=exclude_fields).items():
             if field_name in self.model_fields:
                 setattr(self, field_name, value)
--- tests/structure/domains/entities/test_entity.py
+++ tests/structure/domains/entities/test_entity.py
@@ -1,7 +1,23 @@
+from typing import Optional
 from unittest import TestCase
+from uuid import uuid4
 
+from parameterized import parameterized
+from pydantic import Field
+
+from dddesign.structure.domains.dto import DataTransferObject
 from dddesign.structure.domains.entities import Entity
 from tests.structure.validators import validate_arbitrary_types_allowed, validate_assignment
+
+DEFAULT_VALUE = str(uuid4())
+
+
+class SomeDTO(DataTransferObject):
+    some_field: Optional[str] = None
+
+
+class SomeEntity(Entity):
+    some_field: str = Field(default=DEFAULT_VALUE)
 
 
 class TestEntity(TestCase):
@@ -10,3 +26,21 @@ class TestEntity(TestCase):
 
     def test_assignment(self):
         validate_assignment(Entity)
+
+    @parameterized.expand(
+        (
+            ({}, DEFAULT_VALUE, None),
+            ({'some_field': 'random'}, 'random', None),
+            ({'some_field': 'random'}, DEFAULT_VALUE, {'some_field'}),
+        )
+    )
+    def test_update_method(self, dto_initials, excepted_value, exclude_fields):
+        # Arrange
+        entity = SomeEntity()
+        data = SomeDTO(**dto_initials)
+
+        # Act
+        entity.update(data=data, exclude_fields=exclude_fields)
+
+        # Assert
+        self.assertEqual(entity.some_field, excepted_value)
