89abd106c8f4a4524e57421bdf626132690172cb
davyd_davyd <adavydov@adapty.io>
2024-11-23 21:43:10 +0400

    added ChoiceEnum

--- dddesign/structure/domains/constants/__init__.py
+++ dddesign/structure/domains/constants/__init__.py
@@ -1,1 +1,2 @@
 from .base_enum import BaseEnum
+from .choice_enum import ChoiceEnum
---
+++ dddesign/structure/domains/constants/choice_enum.py
@@ -0,0 +1,15 @@
+from typing import Any, Tuple
+
+from dddesign.structure.domains.constants import BaseEnum
+
+SEPARATOR = '_'
+
+
+class ChoiceEnum(BaseEnum):
+    def get_title(self) -> str:
+        _name = ' '.join(self.name.split(SEPARATOR))
+        return _name.title()
+
+    @classmethod
+    def get_choices(cls) -> Tuple[Tuple[Any, str], ...]:
+        return tuple((value.value, value.get_title()) for value in cls)
