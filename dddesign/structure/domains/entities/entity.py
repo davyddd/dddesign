bb9e9b85c9d50e70cd8c3ab654131418dee863b2
davyd_davyd <adavydov@adapty.io>
2025-01-09 17:27:17 +0300

    relaxed typing requirements of Entity update method

--- dddesign/structure/domains/entities/entity.py
+++ dddesign/structure/domains/entities/entity.py
@@ -2,13 +2,11 @@ from typing import Optional, Set
 
 from pydantic import BaseModel, ConfigDict
 
-from dddesign.structure.domains.dto.dto import DataTransferObject
-
 
 class Entity(BaseModel):
     model_config = ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
 
-    def update(self, data: DataTransferObject, exclude_fields: Optional[Set[str]] = None):
+    def update(self, data: BaseModel, exclude_fields: Optional[Set[str]] = None):
         for field_name, value in data.model_dump(exclude_unset=True, exclude=exclude_fields).items():
             if field_name in self.model_fields:
                 setattr(self, field_name, value)
